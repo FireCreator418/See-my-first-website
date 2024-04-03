70da48a17e7549a65a464112bd1962b43ba87280
FireCreator418 <165873494+FireCreator418@users.noreply.github.com>
2024-04-03 16:35:20 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var number = 10;
+var string = 'Cute cute cute cute dog';
+var isThisDogIsCute = true;
+
+
+document.getElementById('dog').addEventListener('click', function(){
+    alert('Bark Bark!');
+})
